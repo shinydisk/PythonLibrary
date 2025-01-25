85472056ae8ce25ca79ea7f0f9b600824e520857
shinydisk <fshinydisk@proton.me>
2025-01-25 01:35:23 +0100

    Update all

---
+++ Networking/EXEC-Banner.sh
@@ -0,0 +1,14 @@
+    
+    #######################################################
+    #     W A R N I N G  /  A D V E R T I S E M E N T     #
+    #######################################################
+
+        W A R N I N G : This is a private system.
+
+        - Unauthorized access to or use of this system is strictly prohibited.
+        - By continuing, you acknowledge that your use of this system may be 
+        monitored and recorded and you consent to any such monitoring and recording.
+        - Unauthorized users or unauthorized use may subject you to criminal 
+        prosecution and penalties.
+        - Unauthorized access to or use of this system may result in administrative 
+        disciplinary action and civil and criminal penalties.
